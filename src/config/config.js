3791d2bf7c0de09d1354677dbd9cdc47edb99e32
Ivan Ferreira <ivan.marcelo.ferreira@gmail.com>
2026-02-19 21:57:14 -0300

    fix tsconfig; app.ts e server.ts

---
+++ src/config/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  development: {
+    username: 'root',
+    password: 'mysecretpassword',
+    database: 'filmes_2026',
+    host: 'localhost',
+    dialect: 'postgres', // ou mysql
+  },
+};
